7436308a3230708526c49c1bb4c7cb120c33fa29
Nick Cao <nickcao@nichi.co>
2024-06-13 10:47:20 -0400

    use json for crd

--- src/bin/dutlink-crd.rs
+++ src/bin/dutlink-crd.rs
@@ -3,6 +3,6 @@ use kube::CustomResourceExt;
 fn main() {
     print!(
         "{}",
-        serde_yaml::to_string(&dutlink_cli::crd::DutLink::crd()).unwrap()
+        serde_json::to_string(&dutlink_cli::crd::DutLink::crd()).unwrap()
     )
 }
